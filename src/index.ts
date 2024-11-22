1855b0d97bce3d1178c922dd431624cc173f1bfe
The Vimmer <ilyesbouzayen@hotmail.com>
2024-11-22 10:42:20 +0100

    Change the currency in TND

--- src/index.ts
+++ src/index.ts
@@ -91,22 +91,22 @@ const productManager = new ProductManager<Product>();
 try {
 
     // implementation : 
-    const laptop = new Product(1, "Laptop", 999.99, CategoryProduct.ELECTRONICS);
-    const tShirt = new Product(2, "T-Shirt", 19.99, CategoryProduct.CLOTHING);
-    const apple = new FoodProduct(3, "Apple", 0.99, new Date("2024-01-01"));
-    const microsoft = new Product(4, "Microsoft", 999.99, CategoryProduct.ELECTRONICS);
-    const orange = new FoodProduct(5, "Orange", 0.99, new Date("2024-01-01"));
-    const strawberry = new Product(6, "Strawberry", 0.99, CategoryProduct.FOOD);
-    const blackberry = new Product(7, "Blackberry", 0.99, CategoryProduct.FOOD);
-    const raspberry = new Product(8, "Raspberry", 0.99, CategoryProduct.FOOD);
-    const smartphone = new Product(9, "Smartphone", 699.99, CategoryProduct.ELECTRONICS);
-    const jeans = new Product(10, "Jeans", 49.99, CategoryProduct.CLOTHING);
-    const banana = new FoodProduct(11, "Banana", 0.79, new Date("2024-01-05")); 
-    const tablet = new Product(12, "Tablet", 399.99, CategoryProduct.ELECTRONICS);
-    const dress = new Product(13, "Summer Dress", 79.99, CategoryProduct.CLOTHING);
-    const mango = new FoodProduct(14, "Mango", 1.99, new Date("2024-01-03"));
-    const smartwatch = new Product(15, "Smartwatch", 199.99, CategoryProduct.ELECTRONICS);
-    const hoodie = new Product(16, "Hoodie", 39.99, CategoryProduct.CLOTHING);
+    const laptop = new Product(1, "Laptop", 3199.97, CategoryProduct.ELECTRONICS);
+    const tShirt = new Product(2, "T-Shirt", 63.97, CategoryProduct.CLOTHING);
+    const apple = new FoodProduct(3, "Apple", 3.17, new Date("2024-01-01"));
+    const microsoft = new Product(4, "Microsoft", 3199.97, CategoryProduct.ELECTRONICS);
+    const orange = new FoodProduct(5, "Orange", 3.17, new Date("2024-01-01"));
+    const strawberry = new Product(6, "Strawberry", 3.17, CategoryProduct.FOOD);
+    const blackberry = new Product(7, "Blackberry", 3.17, CategoryProduct.FOOD);
+    const raspberry = new Product(8, "Raspberry", 3.17, CategoryProduct.FOOD);
+    const smartphone = new Product(9, "Smartphone", 2239.97, CategoryProduct.ELECTRONICS);
+    const jeans = new Product(10, "Jeans", 159.97, CategoryProduct.CLOTHING);
+    const banana = new FoodProduct(11, "Banana", 2.53, new Date("2024-01-05")); 
+    const tablet = new Product(12, "Tablet", 1279.97, CategoryProduct.ELECTRONICS);
+    const dress = new Product(13, "Summer Dress", 255.97, CategoryProduct.CLOTHING);
+    const mango = new FoodProduct(14, "Mango", 6.37, new Date("2024-01-03"));
+    const smartwatch = new Product(15, "Smartwatch", 639.97, CategoryProduct.ELECTRONICS);
+    const hoodie = new Product(16, "Hoodie", 127.97, CategoryProduct.CLOTHING);
     
    // calls of the methods :
    
